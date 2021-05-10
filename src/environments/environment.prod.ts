60d55b28fe9882274e19d80cb97365d4179e64c1
Philippe Matray <philippe@atypical.consulting>
2021-05-10 20:01:44 +0200

    configure firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  name: 'prod'
+  name: 'prod',
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyBpEC3EM2Tu8Db_QS7RcGUqAgtPupAmNJo',
+      authDomain: 'courseapp-1ac01.firebaseapp.com',
+      projectId: 'courseapp-1ac01',
+      storageBucket: 'courseapp-1ac01.appspot.com',
+      messagingSenderId: '1000527886313',
+      appId: '1:1000527886313:web:74eb94902833427d0b7d78'
+    }
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  name: 'default',
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyBpEC3EM2Tu8Db_QS7RcGUqAgtPupAmNJo',
+      authDomain: 'courseapp-1ac01.firebaseapp.com',
+      projectId: 'courseapp-1ac01',
+      storageBucket: 'courseapp-1ac01.appspot.com',
+      messagingSenderId: '1000527886313',
+      appId: '1:1000527886313:web:74eb94902833427d0b7d78'
+    }
+  }
 };
 
 /*
