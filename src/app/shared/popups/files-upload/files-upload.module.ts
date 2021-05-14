38f3859c81cca5cac8f60d2192513399586741c6
Philippe Matray <philippe@atypical.consulting>
2021-05-14 02:48:36 +0200

    add fileSize pipe

--- src/app/shared/popups/files-upload/files-upload.module.ts
+++ src/app/shared/popups/files-upload/files-upload.module.ts
@@ -8,11 +8,12 @@ import { FilesUploadDirective } from './files-upload.directive';
 import { FilesUploadComponent } from './files-upload.component';
 import { DropZoneDirective } from './directives/drop-zone/drop-zone.directive';
 import { UploadComponent } from './components/upload/upload.component';
+import { FileSizePipe } from './pipes/file-size/file-size.pipe';
 
 
 
 @NgModule({
-  declarations: [FilesUploadDirective, FilesUploadComponent, DropZoneDirective, UploadComponent],
+  declarations: [FilesUploadDirective, FilesUploadComponent, DropZoneDirective, UploadComponent, FileSizePipe],
   imports: [
     CommonModule,
     MatDialogModule,
